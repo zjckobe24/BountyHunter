3bcefb3ba383305673a66db294d48b3a1492315b
zjckobe24 <zhangjckimi@gmail.com>
2015-11-11 21:27:20 -0500

    Initial Commit

---
+++ BountyHunterTests/BountyHunterTests.swift
@@ -0,0 +1,36 @@
+//
+//  BountyHunterTests.swift
+//  BountyHunterTests
+//
+//  Created by junchao zhang on 11/11/15.
+//  Copyright © 2015 junchao zhang. All rights reserved.
+//
+
+import XCTest
+@testable import BountyHunter
+
+class BountyHunterTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
